f37a4c2af74f642e084f98942beb49638ac08250
Robert Hill <codingbyrobert@gmail.com>
2015-10-12 21:44:49 -0700

    added more pics to js

--- app.js
+++ app.js
@@ -8,11 +8,35 @@ var getRandomNum = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var spider1 = new Photo('<img src = "img/spider-1.jpg" />');
-var spider2 = new Photo('<img src= "img/spider-2.jpg" />');
+var spider1 = new Photo('<img src="img/spider-1.jpg" />');
+var spider2 = new Photo('<img src="img/spider-2.jpg" />');
+//spider
+var snake1 = new Photo('<img src="img/snake-1.jpg" />');
+var snake2 = new Photo('<img src="img/snake-2.jpg" />');
+//snake
+var dog1 = new Photo('<img src="img/dog-1.jpg" />');
+//dog
+//dog
+var clown1 = new Photo('<img src="img/clown-1.jpg" />');
+var clown3 = new Photo('<img src="img/clown-3.jpg" />');
+//clown
+var germ1 = new Photo('<img src="img/germ-1.jpg" />');
+//germ
+//germ
+var heights1 = new Photo('<img src="img/heights-1.jpg" />');
+var heights2 = new Photo('<img src="img/heights-2.jpg" />');
+var heights3 = new Photo('<img src="img/heights-3.jpg" />');
+var kitten1 = new Photo('<img src="img/kitten-1.jpg" />');
+var kitten2 = new Photo('<img src="img/kitten-2.jpg" />');
+var lightning1 = new Photo('<img src="img/lightning-1.jpg" />');
+var lightning2 = new Photo('<img src="img/lightning-2.jpg" />');
 
-document.getElementById('picFrame1').innerHTML = '<img src = "img/spider-1.jpg" />';
-document.getElementById('picFrame2').innerHTML = '<img src = "img/spider-1.jpg" />';
+var needle1 = new Photo('<img src="img/needles-1.jpg" />');
+var needle2 = new Photo('<img src="img/needles-2.jpg" />');
+var needle3 = new Photo('<img src="img/needles-3.jpg" />');
+
+document.getElementById('picFrame1').innerHTML = '<img src="img/dog-1.jpg" />';
+document.getElementById('picFrame2').innerHTML = '<img src = "img/snake-1.jpg" />';
 
 var ratePic =function(e) {
     e.preventDefault();
@@ -21,11 +45,17 @@ var ratePic =function(e) {
     console.log('i1 = ' +  i1 +' i2 = ' + i2)
     randPic1 = photoArray[i1];
     randPic2 = photoArray[i2];
-    if (randPic1 != null && randPic2 != null ){
+    if (randPic1 != null && randPic2 != null){
+        if (randPic1 === randPic2){
+            i1 = getRandomNum(0, photoArray.length - 1);
+            //i2 = getRandomNum(0, photoArray.length - 1);
+            //console.log('i1 = ' +  i1 +' i2 = ' + i2)
+            randPic1 = photoArray[i1];
+            //randPic2 = photoArray[i2];
+        }
+        console.log('Button ratePic function')
         document.getElementById('picFrame1').innerHTML = randPic1.picLink;
         document.getElementById('picFrame2').innerHTML = randPic2.picLink;
-
-        console.log('Button ratePic function')
 
     }
 
