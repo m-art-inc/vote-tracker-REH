6a5edf9affa2a477e75cf3bad520d1884dead0c2
Robert Hill <codingbyrobert@gmail.com>
2015-10-14 11:50:14 -0700

    added canvas to html

--- app.js
+++ app.js
@@ -13,46 +13,45 @@ var Tracker = {
 var Photo = function(fear,picLink) {
   this.fear = fear;
   this.picLink =  picLink;
-  //this.picRating = null; //not used yet, might be replaced
   photoArray.push(this);
 }
 var getRandomNum = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-var clown1 = new Photo('clown','<img src="img/clown-1.jpg" />');
-var clown2 = new Photo('clown', '<img src="img/clown-2.jpg" />')
-var clown3 = new Photo('clown','<img src="img/clown-3.jpg" />');
-var clown4 = new Photo('clown', '<img src="img/clown-4.png" />');
+var clown1 = new Photo('clown','<img title="Pennywise" src="img/clown-1.jpg" />');
+var clown2 = new Photo('clown', '<img title="Ronald Mcdonald" src="img/clown-2.jpg" />')
+var clown3 = new Photo('clown','<img title="Circus Clowns" src="img/clown-3.jpg" />');
+var clown4 = new Photo('clown', '<img title="Twisty" src="img/clown-4.png" />');
 
-var dog1 = new Photo('dog','<img src="img/dog-1.jpg" />');
-var dog2 = new Photo('dog', '<img src="img/dog-2.jpg" />');
-var dog3 = new Photo('dog', '<img src="img/dog-3.jpg" />');
+var dog1 = new Photo('dog','<img title="Irish WolfHound" src="img/dog-1.jpg" />');
+var dog2 = new Photo('dog', '<img title="Crested Hairless" src="img/dog-2.jpg" />');
+var dog3 = new Photo('dog', '<img title="Golden Retriever" src="img/dog-3.jpg" />');
 
-var germ1 = new Photo('germ','<img src="img/germ-1.jpg" />');
-var germ2 = new Photo('germ','<img src="img/germ-2.jpg" />');
-var germ3 = new Photo('germ','<img src="img/germ-3.jpg" />');
+var germ1 = new Photo('germ','<img title="Common Cold" src="img/germ-1.jpg" />');
+var germ2 = new Photo('germ','<img title="Germs" src="img/germ-2.jpg" />');
+var germ3 = new Photo('germ','<img title="Germs" src="img/germ-3.jpg" />');
 
-var heights1 = new Photo('heights','<img src="img/heights-1.jpg" />');
-var heights2 = new Photo('heights','<img src="img/heights-2.jpg" />');
-var heights3 = new Photo('heights','<img src="img/heights-3.jpg" />');
+var heights1 = new Photo('heights','<img title="heights" src="img/heights-1.jpg" />');
+var heights2 = new Photo('heights','<img title="heights" src="img/heights-2.jpg" />');
+var heights3 = new Photo('heights','<img title="heights" src="img/heights-3.jpg" />');
 
-var kitten1 = new Photo('kitten','<img src="img/kitten-1.jpg" />');
-var kitten2 = new Photo('kitten','<img src="img/kitten-2.jpg" />');
+var kitten1 = new Photo('kitten','<img title="Kittens" src="img/kitten-1.jpg" />');
+var kitten2 = new Photo('kitten','<img title="Kittens" src="img/kitten-2.jpg" />');
 
-var lightning1 = new Photo('storm','<img src="img/lightning-1.jpg" />');
-var lightning2 = new Photo('storm','<img src="img/lightning-2.jpg" />');
+var lightning1 = new Photo('storm','<img title="lightning" src="img/lightning-1.jpg" />');
+var lightning2 = new Photo('storm','<img title="lightning" src="img/lightning-2.jpg" />');
 
-var needle1 = new Photo('needle','<img src="img/needles-1.jpg" />');
-var needle2 = new Photo('needle','<img src="img/needles-2.jpg" />');
-var needle3 = new Photo('needle','<img src="img/needles-3.jpg" />');
+var needle1 = new Photo('needle','<img title="Needle" src="img/needles-1.jpg" />');
+var needle2 = new Photo('needle','<img title="Needle" src="img/needles-2.jpg" />');
+var needle3 = new Photo('needle','<img title="Needle" src="img/needles-3.jpg" />');
 
-var snake1 = new Photo('snake','<img src="img/snake-1.jpg" />');
-var snake2 = new Photo('snake','<img src="img/snake-2.jpg" />');
+var snake1 = new Photo('snake','<img title="Snake" src="img/snake-1.jpg" />');
+var snake2 = new Photo('snake','<img title="Snake" src="img/snake-2.jpg" />');
 //var snake3 = new Photo ('snake', '<img src="img/snake-3.jpg" />');
 
-var spider1 = new Photo('spider','<img src="img/spider-1.jpg" />');
-var spider2 = new Photo('spider','<img src="img/spider-2.jpg" />');
-var spider3 = new Photo('spider', '<img src="img/spider-3.jpg" />')
+var spider1 = new Photo('spider','<img title="Spider" src="img/spider-1.jpg" />');
+var spider2 = new Photo('spider','<img title="Spider" src="img/spider-2.jpg" />');
+var spider3 = new Photo('spider', '<img title="Spider" src="img/spider-3.jpg" />')
 
 function randomPic() {
    var i1 = getRandomNum(0, photoArray.length -1);
@@ -67,8 +66,8 @@ function randomPic() {
             i2 = getRandomNum(0, photoArray.length -1);
             randPic1 = photoArray[i1];
             randPic2 = photoArray[i2];
-            console.log('photos fear are the same get new random');
-            console.log('i1 = ' +  i1 +' i2 = ' + i2);
+            // console.log('photos fear are the same get new random');
+            // console.log('i1 = ' +  i1 +' i2 = ' + i2);
         }
         //console.log('Random pic function')
         var pic1 = document.getElementById('picFrame1');
@@ -85,7 +84,7 @@ function randomPic() {
 var ratePic = function(e,picFrame) {
      e.preventDefault();
     if (picFrame === 'picFrame1') {
-            console.log('pic 1 clicked');
+            // console.log('pic 1 clicked');
             var pic = document.getElementById('picFrame1');
             console.log(pic.className);
             switch (pic.className){
@@ -93,35 +92,35 @@ var ratePic = function(e,picFrame) {
                     Tracker.clownVote += 1;
                     break;
                 case 'dog':
-                    Tracker.dogVote +=1;
+                    Tracker.dogVote += 1;
                     break;
                 case 'germ':
-                    Tracker.germVote +=1;
+                    Tracker.germVote += 1;
                     break;
                 case 'heights':
-                    Tracker.heightsVote +=1;
+                    Tracker.heightsVote += 1;
                     break;
                 case 'kitten':
-                    Tracker.kittenVote +=1;
+                    Tracker.kittenVote += 1;
                     break;
                 case 'lightning':
-                    Tracker.lightningVote +=1;
+                    Tracker.lightningVote += 1;
                     break;
                 case 'needle':
-                    Tracker.needleVote +=1;
+                    Tracker.needleVote += 1;
                     break;
                 case 'snake':
-                    Tracker.snakeVote +=1;
+                    Tracker.snakeVote += 1;
                     break;
                 case 'spider':
-                    Tracker.spiderVote +=1;
+                    Tracker.spiderVote += 1;
                     break;
 
             }
             randomPic();
     }
     else if (picFrame === 'picFrame2'){
-        console.log('pic 2 clicked');
+        // console.log('pic 2 clicked');
         var pic = document.getElementById('picFrame2');
             console.log(pic.className);
             switch (pic.className){
@@ -129,35 +128,34 @@ var ratePic = function(e,picFrame) {
                     Tracker.clownVote += 1;
                     break;
                 case 'dog':
-                    Tracker.dogVote +=1;
+                    Tracker.dogVote += 1;
                     break;
                 case 'germ':
-                    Tracker.germVote +=1;
+                    Tracker.germVote += 1;
                     break;
                 case 'heights':
-                    Tracker.heightsVote +=1;
+                    Tracker.heightsVote += 1;
                     break;
                 case 'kitten':
-                    Tracker.kittenVote +=1;
+                    Tracker.kittenVote += 1;
                     break;
                 case 'lightning':
-                    Tracker.lightningVote +=1;
+                    Tracker.lightningVote += 1;
                     break;
                 case 'needle':
-                    Tracker.needleVote +=1;
+                    Tracker.needleVote += 1;
                     break;
                 case 'snake':
-                    Tracker.snakeVote +=1;
+                    Tracker.snakeVote += 1;
                     break;
                 case 'spider':
-                    Tracker.spiderVote +=1;
+                    Tracker.spiderVote += 1;
                     break;
 
             }
             randomPic();
     }
 }
-
 randomPic();
 
 document.getElementById('picFrame1').addEventListener('click', function(){
